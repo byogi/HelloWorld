951fc212918bc4ea3d3ac4bb28a25ebd02891def
byogi <Bryson@hisairness.hawaii.rr.com>
2014-08-09 19:32:00 -1000

    Hello World
    Added Some stuff

--- HelloWorld/src/HelloWorldGitHub.java
+++ HelloWorld/src/HelloWorldGitHub.java
@@ -2,7 +2,7 @@
 public class HelloWorldGitHub {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World, Test for github");
+		System.out.println("Hello World, Test for github chee mac");
 	}
 
 }
